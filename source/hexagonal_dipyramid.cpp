88e7cce42c8e326c68efb622cde08caca62d732c
NIRMAL MANOJ C <nimmumanoj@gmail.com>
2021-03-16 03:49:06 +0530

    Formatted models

--- source/hexagonal_dipyramid.cpp
+++ source/hexagonal_dipyramid.cpp
@@ -1,8 +1,6 @@
 #include "hexagonal_dipyramid.h"
 
-
 #define vt(tmp) tmp.x, tmp.y, tmp.z
-
 
 HexagonalDipyramid::HexagonalDipyramid(float x, float y, color_t color)
 {
@@ -46,7 +44,7 @@ HexagonalDipyramid::HexagonalDipyramid(float x, float y, color_t color)
         vt(v6), // triangle 6: begin
         vt(v1),
         vt(v7), // triangle 6: end
-        
+
         vt(v1), // triangle 7: begin
         vt(v2),
         vt(v8), // triangle 7: end
@@ -64,32 +62,34 @@ HexagonalDipyramid::HexagonalDipyramid(float x, float y, color_t color)
         vt(v8), // triangle 11: end
         vt(v6), // triangle 12: begin
         vt(v1),
-        vt(v8)  // triangle 12: end
+        vt(v8) // triangle 12: end
     };
 
     // this->object = create3DObject(GL_TRIANGLES, 12*3, vertex_buffer_data, color, GL_FILL);
 
-    static GLfloat color_buffer_data[12*3*3];
-    for(int i = 0; i < 12*3*3; i+=9)
+    static GLfloat color_buffer_data[12 * 3 * 3];
+    for (int i = 0; i < 12 * 3 * 3; i += 9)
     {
         color_buffer_data[i] = rand() % 256;
         color_buffer_data[i] /= 256.0;
         color_buffer_data[i] = rand() % 256;
         color_buffer_data[i] /= 256.0;
-        for (int j = i+3; j < i + 9; j+=3){
+        for (int j = i + 3; j < i + 9; j += 3)
+        {
             color_buffer_data[j] = color_buffer_data[i];
         }
-        color_buffer_data[i+1] = rand() % 256;
-        color_buffer_data[i+1] /= 256.0;
-        for (int j = i+4; j < i + 9; j+=3){
-            color_buffer_data[j] = color_buffer_data[i+1];
+        color_buffer_data[i + 1] = rand() % 256;
+        color_buffer_data[i + 1] /= 256.0;
+        for (int j = i + 4; j < i + 9; j += 3)
+        {
+            color_buffer_data[j] = color_buffer_data[i + 1];
         }
-        color_buffer_data[i+2] = rand() % 256;
-        color_buffer_data[i+2] /= 256.0;
-        for (int j = i+5; j < i + 9; j+=3){
-            color_buffer_data[j] = color_buffer_data[i+2];
+        color_buffer_data[i + 2] = rand() % 256;
+        color_buffer_data[i + 2] /= 256.0;
+        for (int j = i + 5; j < i + 9; j += 3)
+        {
+            color_buffer_data[j] = color_buffer_data[i + 2];
         }
     }
-    this->object = create3DObject(GL_TRIANGLES, 12*3, vertex_buffer_data, color_buffer_data, GL_FILL);
-
+    this->object = create3DObject(GL_TRIANGLES, 12 * 3, vertex_buffer_data, color_buffer_data, GL_FILL);
 }
